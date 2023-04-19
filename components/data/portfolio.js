67ca2e4e0bbf18602ffd9c220e392edb8e86f28c
arman <armman.mousavi20@gmail.com>
2023-04-19 02:14:50 -0700

    update1

--- components/data/portfolio.js
+++ components/data/portfolio.js
@@ -1,6 +1,52 @@
 const portfolio = [
-  {
+    {
     id: "01",
+    title: "Calculator App",
+    img: "/images/portfolio6.png",
+    category: "reactjs",
+    keyword: ["app", "reactjs"],
+    liveUrl: "https://calcultor-with-reactjs.vercel.app/",
+    github:"https://github.com/armanmousavi20/calcultor-with-reactjs",
+  },  
+  {
+    id: "02",
+    title: "petshop landing page",
+    img: "/images/portfolio12.png",
+    category: "nextjs",
+    keyword: ["nextjs", "ui", "tailwind"],
+    liveUrl: "https://petshop-nextjs-theta.vercel.app/",
+    github:"https://github.com/armanmousavi20/petshop-nextjs",
+  },
+  {
+    id: "03",
+    title: "Nike ecommerce site",
+    img: "/images/portfolio11.png",
+    category: "reactjs",
+    keyword: ["reactjs", "reduxtoolkit", "typescript"],
+    liveUrl: "https://nike-store-liard.vercel.app/",
+    github:"https://github.com/armanmousavi20/Nike-store",
+
+  },
+  {
+    id: "04",
+    title: "quiz app",
+    img: "/images/portfolio7.png",
+    category: "reactjs",
+    keyword: ["app", "reactjs", "reactjs"],
+    liveUrl: "https://quiz-app-reactjs-olive.vercel.app/",
+    github:"https://github.com/armanmousavi20/quiz-app-reactjs",
+  },
+  {
+    id: "05",
+    title: "gymnastic site",
+    img: "/images/portfolio4.png",
+    category: "reactjs",
+    keyword: ["web", "ui", "reactjs"],
+    liveUrl: "https://gymnastic-site-reactjs.vercel.app/",
+    github:"https://github.com/armanmousavi20/gymnastic-site-reactjs",
+  },
+  {
+    id: "06",
     title: "Metaverse site",
     img: "/images/portfolio1.png",
     category: "nextjs",
@@ -10,7 +56,7 @@ const portfolio = [
   },
 
   {
-    id: "02",
+    id: "07",
     title: "Wedding photography",
     img: "/images/portfolio2.png",
     category: "nextjs",
@@ -20,7 +66,7 @@ const portfolio = [
   },
 
   {
-    id: "03",
+    id: "08",
     title: "social media ui",
     img: "/images/portfolio3.png",
     category: "reactjs",
@@ -28,19 +74,8 @@ const portfolio = [
     liveUrl: "https://social-media-reactjs-smoky.vercel.app/profile",
     github:"https://github.com/armanmousavi20/social-media-reactjs",
   },
-
   {
-    id: "04",
-    title: "gymnastic site",
-    img: "/images/portfolio4.png",
-    category: "reactjs",
-    keyword: ["web", "ui", "reactjs"],
-    liveUrl: "https://gymnastic-site-reactjs.vercel.app/",
-    github:"https://github.com/armanmousavi20/gymnastic-site-reactjs",
-  },
-
-  {
-    id: "05",
+    id: "09",
     title: "Online educational site",
     img: "/images/portfolio5.png",
     category: "reactjs",
@@ -48,29 +83,8 @@ const portfolio = [
     liveUrl: "https://educational-website-with-reactjs.vercel.app/",
     github:"https://github.com/armanmousavi20/educational-website-with-reactjs",
   },
-
   {
-    id: "06",
-    title: "Calculator App",
-    img: "/images/portfolio6.png",
-    category: "reactjs",
-    keyword: ["app", "reactjs"],
-    liveUrl: "https://calcultor-with-reactjs.vercel.app/",
-    github:"https://github.com/armanmousavi20/calcultor-with-reactjs",
-  },
-
-  {
-    id: "07",
-    title: "quiz app",
-    img: "/images/portfolio7.png",
-    category: "reactjs",
-    keyword: ["app", "reactjs", "reactjs"],
-    liveUrl: "https://quiz-app-reactjs-olive.vercel.app/",
-    github:"https://github.com/armanmousavi20/quiz-app-reactjs",
-  },
-
-  {
-    id: "08",
+    id: "10",
     title: "movie site",
     img: "/images/portfolio8.png",
     category: "reactjs",
@@ -81,7 +95,7 @@ const portfolio = [
   },
 
   {
-    id: "09",
+    id: "11",
     title: "weather app",
     img: "/images/portfolio9.png",
     category: "reactjs",
@@ -91,33 +105,13 @@ const portfolio = [
 
   },
     {
-    id: "10",
+    id: "12",
     title: "portfolio site",
     img: "/images/portfolio10.png",
     category: "nextjs",
     keyword: ["web", "ui", "nextjs"],
-    liveUrl: "#",
+    liveUrl: "https://portfolio-nextjs-nine-inky.vercel.app/",
     github:"https://github.com/armanmousavi20/portfolio-nextjs",
-
-  },
-  {
-    id: "11",
-    title: "Nike ecommerce site",
-    img: "/images/portfolio11.png",
-    category: "reactjs",
-    keyword: ["reactjs", "reduxtoolkit", "typescript"],
-    liveUrl: "https://nike-store-liard.vercel.app/",
-    github:"https://github.com/armanmousavi20/Nike-store",
-
-  },
-  {
-    id: "12",
-    title: "petshop landing page",
-    img: "/images/portfolio12.png",
-    category: "nextjs",
-    keyword: ["nextjs", "ui", "tailwind"],
-    liveUrl: "https://petshop-nextjs-theta.vercel.app/",
-    github:"https://github.com/armanmousavi20/petshop-nextjs",
   },
 ];
 
